062183201bbd9dd8758f0cc38c293953e7a3d820
nikolay <nikcbg@yahoo.com>
2019-01-22 08:58:55 +0200

    add file

---
+++ scripts/provision.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install xorg
+apt-get install openbox
+apt-get install -y x11-apps
+~                                                                               
+~                                                                               
+~                                  
